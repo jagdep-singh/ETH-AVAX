65c53443d89e10fe36f45c97f8d0c44e1214dd97
Jagdeep Singh <83873014+jagdep-singh@users.noreply.github.com>
2023-07-19 13:11:40 +0530

    Update JazToken.sol

--- Mod3_Assingment/JazToken.sol
+++ Mod3_Assingment/JazToken.sol
@@ -1,1 +1,30 @@
-oviwbviusbd
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract JazToken is ERC20 {
+    address private owner;
+
+    constructor() ERC20("MyToken", "JAZ") {
+        owner = msg.sender;
+    }
+
+    modifier ownerOnly() {
+        require(msg.sender == owner, "Only the contract owner can call this function");
+        _;
+    }
+
+    function mint(address account, uint256 amount) external ownerOnly {
+        _mint(account, amount);
+    }
+
+    function transfer(address recipient, uint256 amount) public virtual override returns (bool) {
+        _transfer(_msgSender(), recipient, amount);
+        return true;
+    }
+
+    function burn(uint256 amount) external {
+        _burn(msg.sender, amount);
+    }
+}
